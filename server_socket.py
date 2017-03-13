e88e3b267b424afd1fa7e3c8edae05c891e683df
George Ceja <net.george.sky@gmail.com>
2017-03-12 21:50:47 -0700

    Create server_socket.py

---
+++ server_socket.py
@@ -0,0 +1,16 @@
+import sockets
+import threading
+
+home_ip = 
+home_port = 
+
+server = socket..........
+server.bind(...)
+server.listen(...)
+
+def client_handler(client_socket):
+  pass
+  
+...
+...
+...
